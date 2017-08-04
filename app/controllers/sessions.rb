d1b55c80f99dffb59435cb1ff4f9f7278fe22e8b
Dominique Goncalves <email": null>
2017-08-04 17:42:26 -0500

    re push code to sign in wiuth email, at least currently

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -8,7 +8,7 @@ end
 
 post '/sessions' do
   p params[:users]
-  @user = User.find_by(username: params[:users][:username])
+  @user = User.find_by(email: params[:users][:email])
 
   if @user && @user.authenticate?(params[:users][:password_text])
     session[:user_id] = @user.id
